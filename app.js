7ed700c3de05633057b3a543722690be71c14969
DoorTask <doortasktech@DoorTasks-MacBook-Pro.local>
2021-04-19 18:43:35 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('CICD SAMPLE'));
+app.get('/', (req, res) => res.send('We have created a AWS Pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
